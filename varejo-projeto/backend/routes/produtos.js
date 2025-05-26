7b18b19c8aeb42b7749e7d79bc0665b60b98e92c
Levi Henrique <83926172+levi985@users.noreply.github.com>
2025-05-26 17:58:35 +0000

    Delete varejo-projeto directory

--- varejo-projeto/backend/routes/produtos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.json([{ id: 1, nome: "Camiseta", preco: 49.99 }]);
-});
-
-module.exports = router;
-
--- varejo-projeto/backend/routes/vendas.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.json([{ id: 1, produto: "Notebook", total: 2999.99 }]);
-});
-
-module.exports = router;
-
